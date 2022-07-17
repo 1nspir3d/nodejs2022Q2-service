09ed68d9ce15eeed305d29201196d5d367470ef8
DavydVeremchuk <davyd.v@code-care.pro>
2022-07-17 23:55:17 +0300

    doc: update readme & add swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
