ec10b35e52ba0c2963ae07a09ce37e2e7258def4
DavydVeremchuk <davyd.v@code-care.pro>
2022-08-07 18:00:00 +0300

    feat: add logger and filter

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { dirname, join } from 'path';
 import { parse } from 'yaml';
 import { readFile } from 'fs/promises';
+import { LoggerService } from './core/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggerService(),
+  });
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
